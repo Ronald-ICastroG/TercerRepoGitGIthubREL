56914542cb06d109a6efb4ceb73ed726c9e57ee9
Ronald-ICastroG_Acer <ronaldivan.1210868@correo.uis.edu.co>
2026-02-02 01:19:23 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tercer_repo',                                  # Nombre del paquete
+    version='0.1',                                       # Versión inicial del paquete
+    packages=find_packages(),                            # Paquetes a incluir
+    description="un paquete pip simple de saludo",       # Breve descripción del paquete
+    author='Ronald Castro',                              # Tu nombre
+    author_email='ronald@platzi.com',                    # Tu correo electrónico
+    url='https://github.com/R-ICastroG/tercer_repo',     # URL del repositorio del proyecto
+)
---
+++ tercer_repo/__init__.py
@@ -0,0 +1,2 @@
+def saludar(nombre):
+    return "¡Hola ",+ nombre+ " ! Bienvenido a 'tercer_repo' este es mi primer pip!."
