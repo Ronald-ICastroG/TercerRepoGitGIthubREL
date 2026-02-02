a76592104a4aa103662483b71fca1d15be5f2b3b
Ronald-ICastroG_Acer <ronaldivan.1210868@correo.uis.edu.co>
2026-02-02 01:36:14 -0500

    modificacion_nombre_proyecto

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tercer_repo',                                  # Nombre del paquete
+    name='tercerrepo',                                  # Nombre del paquete
     version='0.1',                                       # Versión inicial del paquete
     packages=find_packages(),                            # Paquetes a incluir
     description="un paquete pip simple de saludo",       # Breve descripción del paquete
     author='Ronald Castro',                              # Tu nombre
     author_email='ronald@platzi.com',                    # Tu correo electrónico
-    url='https://github.com/R-ICastroG/tercer_repo',     # URL del repositorio del proyecto
+    url='https://github.com/R-ICastroG/tercerrepo',     # URL del repositorio del proyecto
 )
